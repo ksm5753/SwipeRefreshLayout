91bb9e65af1d7a6b8dd07ffcf47cbe6b66cd143f
강석문 <pita@gangseogmun-ui-MacBook-Pro.local>
2017-08-14 12:33:31 +0900

    first commit

---
+++ app/src/main/java/com/example/pita/swiperefreshlayout/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.pita.swiperefreshlayout;
+
+import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private SwipeRefreshLayout swipeLayout;
+    static final String[] LIST_MENU = {"LIST1", "LIST2", "LIST3"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, LIST_MENU);
+
+        ListView listview = (ListView) findViewById(R.id.listView);
+        listview.setAdapter(adapter);
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                String strText = (String) parent.getItemAtPosition(position);
+                Toast.makeText(MainActivity.this, strText, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipeLayout);
+        swipeLayout.setColorSchemeResources(R.color.colorPrimary, R.color.colorAccent);
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeLayout.setRefreshing(false);
+            }
+        });
+    }
+}
